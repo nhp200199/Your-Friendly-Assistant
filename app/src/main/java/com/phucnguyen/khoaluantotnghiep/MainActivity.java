998b4b3def0b77b19409d39385fe09890ce0109a
Phuc Nguyen <nhp200199@gmail.com>
2021-01-05 10:43:12 +0700

    setup navigation with action bar

--- app/src/main/java/com/phucnguyen/khoaluantotnghiep/MainActivity.java
+++ app/src/main/java/com/phucnguyen/khoaluantotnghiep/MainActivity.java
@@ -1,7 +1,9 @@
 package com.phucnguyen.khoaluantotnghiep;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
@@ -9,20 +11,42 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private AppBarConfiguration appBarConfiguration;
+    private NavHostFragment navHostFragment;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setUpNavigation();
+        navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.home_fragment,
+                R.id.find_products_fragment,
+                R.id.hot_products_fragment,
+                R.id.setting_fragment
+        ).build();
+
+        setUpBotNavigation();
+        setUpActionBar();
     }
 
-    private void setUpNavigation() {
+    private void setUpActionBar() {
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+
+    private void setUpBotNavigation() {
         BottomNavigationView bottomNav = findViewById(R.id.bttm_nav);
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(bottomNav,
                 navHostFragment.getNavController());
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
