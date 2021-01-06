2f88f72789b3abff7ef4f1128a1d1ad908300cef
Phuc Nguyen <nhp200199@gmail.com>
2021-01-06 15:26:35 +0700

    add LogInFragment + necessary resources for it

---
+++ app/src/main/java/com/phucnguyen/khoaluantotnghiep/LogInFragment.java
@@ -0,0 +1,23 @@
+package com.phucnguyen.khoaluantotnghiep;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LogInFragment extends Fragment {
+
+    public LogInFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log_in, container, false);
+    }
+}
