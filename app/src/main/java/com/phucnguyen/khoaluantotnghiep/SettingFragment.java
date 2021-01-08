a678cc7b86293ce73a3f92b8249d9935f75ded58
Phuc Nguyen <nhp200199@gmail.com>
2021-01-08 11:56:03 +0700

    modify to use ChildFragmentManager

--- app/src/main/java/com/phucnguyen/khoaluantotnghiep/SettingFragment.java
+++ app/src/main/java/com/phucnguyen/khoaluantotnghiep/SettingFragment.java
@@ -1,14 +1,18 @@
 package com.phucnguyen.khoaluantotnghiep;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.preference.PreferenceFragmentCompat;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SettingFragment extends Fragment {
 
@@ -20,9 +24,9 @@ public class SettingFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (savedInstanceState == null)
-            requireActivity().getSupportFragmentManager()
+            getChildFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.settingPreferenceContainer, new SettingPreferenceFragment())
+                    .add(R.id.settingPreferenceContainer, new SettingPreferenceFragment())
                     .commit();
     }
 
