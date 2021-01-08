70e5222b3348b7efaf228954ef4af7fa14fdc229
Phuc Nguyen <nhp200199@gmail.com>
2021-01-08 16:52:26 +0700

    navigate to OnBoardingFragment

--- app/src/main/java/com/phucnguyen/khoaluantotnghiep/SettingPreferenceFragment.java
+++ app/src/main/java/com/phucnguyen/khoaluantotnghiep/SettingPreferenceFragment.java
@@ -1,10 +1,15 @@
 package com.phucnguyen.khoaluantotnghiep;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.navigation.Navigation;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceScreen;
 
 public class SettingPreferenceFragment extends PreferenceFragmentCompat {
     @Override
@@ -16,5 +21,18 @@ public class SettingPreferenceFragment extends PreferenceFragmentCompat {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         getListView().setOverScrollMode(View.OVER_SCROLL_NEVER);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        switch (preference.getKey()) {
+            case "instruction":
+                Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
+                        .navigate(R.id.action_setting_fragment_to_on_boarding_fragment);
+                return true;
+
+            default: return super.onPreferenceTreeClick(preference);
+        }
+
     }
 }
