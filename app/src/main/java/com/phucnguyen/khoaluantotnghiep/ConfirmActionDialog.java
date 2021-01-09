2d82a78b06c3027af92c2df448e3cdee918448fb
Phuc Nguyen <nhp200199@gmail.com>
2021-01-09 12:04:40 +0700

    set confirmation for the choice of user of disabling the notification

---
+++ app/src/main/java/com/phucnguyen/khoaluantotnghiep/ConfirmActionDialog.java
@@ -0,0 +1,67 @@
+package com.phucnguyen.khoaluantotnghiep;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class ConfirmActionDialog extends DialogFragment {
+    interface onConfirmActionListener {
+        void onPositiveConfirmed();
+        void onNegativeConfirmed();
+    }
+
+    private onConfirmActionListener listener;
+
+    private String message;
+    private String positiveAction;
+    private String negativeAction;
+
+    public ConfirmActionDialog(String message, String positiveAction, String negativeAction) {
+        this.message = message;
+        this.positiveAction = positiveAction;
+        this.negativeAction = negativeAction;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+        builder.setMessage(message)
+                .setPositiveButton(positiveAction, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (listener != null)
+                            listener.onPositiveConfirmed();
+                    }
+                })
+                .setNegativeButton(negativeAction, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (listener != null)
+                            listener.onNegativeConfirmed();
+                    }
+                });
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            listener = (onConfirmActionListener) getParentFragment();
+        } catch (ClassCastException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/phucnguyen/khoaluantotnghiep/SettingPreferenceFragment.java
+++ app/src/main/java/com/phucnguyen/khoaluantotnghiep/SettingPreferenceFragment.java
@@ -1,20 +1,22 @@
 package com.phucnguyen.khoaluantotnghiep;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.navigation.Navigation;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.PreferenceManager;
-import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
 
-public class SettingPreferenceFragment extends PreferenceFragmentCompat {
+public class SettingPreferenceFragment extends PreferenceFragmentCompat
+        implements ConfirmActionDialog.onConfirmActionListener {
+    private SwitchPreference notificationPref;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+        notificationPref = findPreference("notification_pref");
     }
 
     @Override
@@ -26,13 +28,30 @@ public class SettingPreferenceFragment extends PreferenceFragmentCompat {
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
         switch (preference.getKey()) {
-            case "instruction":
+            case "instruction_pref":
                 Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
                         .navigate(R.id.action_setting_fragment_to_on_boarding_fragment);
                 return true;
+            case "notification_pref":
+                if (!notificationPref.isChecked())
+                    new ConfirmActionDialog("Đã tắt thông báo. Bạn sẽ không nhận được thông tin về giá của các sản phẩm. Bạn có muốn bật lại không?"
+                            , "Nhận thông báo"
+                            , "Vẫn tiếp tục")
+                            .show(getChildFragmentManager(), null);
+                return true;
 
-            default: return super.onPreferenceTreeClick(preference);
+            default:
+                return super.onPreferenceTreeClick(preference);
         }
+    }
 
+    @Override
+    public void onPositiveConfirmed() {
+        notificationPref.setChecked(true);
+    }
+
+    @Override
+    public void onNegativeConfirmed() {
+        notificationPref.setChecked(false);
     }
 }
